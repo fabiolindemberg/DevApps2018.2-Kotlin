47aa3e8612172b384a0cf8c1ac704ce1f3a4693c
fabiolindemberg <fabiolindemberg@gmail.com>
2019-01-06 08:37:14 -0300

    Adicionando Frament para exibir o resultado da seleção

--- app/src/main/java/appfinalkotlin/fabiolindemberg/com/br/appfinalkotlin/MainActivity.kt
+++ app/src/main/java/appfinalkotlin/fabiolindemberg/com/br/appfinalkotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_result.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,7 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         if( resultCode == Activity.RESULT_OK){
             if( data != null){
-                etResult.text = data.getStringExtra(RESULT_KEY)
+                //etResult.text = data.getStringExtra(RESULT_KEY)
+                fragment_text.text = data.getStringExtra(RESULT_KEY)
             }
         }
     }
